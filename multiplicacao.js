b4bea9a4bee5d843a25c46cf6755cab7b01bc843
Camila Gaertner <61475424+camilagaertner@users.noreply.github.com>
2020-07-03 15:53:22 -0300

    Multiplica o número digitado por 3

---
+++ multiplicacao.js
@@ -0,0 +1,10 @@
+
+    var cont, num, result;
+    
+    for (cont = 1; cont <= 5; cont++) {
+        window.alert("Informe um número");
+        num = window.prompt('Enter a value for num');
+        result = num * 3;
+        window.alert("Resultado da Multiplicação por três: " + result);
+    }
+    window.alert("Fim do Algoritmo");
